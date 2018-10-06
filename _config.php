f68b49b80528f48e10b25853a44444321ef0d057
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2018-10-06 12:01:44 +0300

    SS4 upgrade: Config changes.

--- _config.php
+++ _config.php
@@ -1,8 +1,8 @@
 <?php
 
+namespace UncleCheese\Dashboard;
+
 $dir = basename(dirname(__FILE__));
 if($dir != "dashboard") {
 	user_error('Dashboard: Directory name must be "dashboard" (currently "'.$dir.'")',E_USER_ERROR);
 }
-
-LeftAndMain::require_css("dashboard/css/dashboard_icon.css");
